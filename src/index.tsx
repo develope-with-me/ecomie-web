b485d2516fbdddd9f9af6e6b04d368996553bf86
Morelle-Olivia <hotoumorelle@gmail.com>
2024-09-25 15:08:01 +0100

    Ecomie setUp React App

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {document} from "postcss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = window.document.getElementById('root');
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement); // Create the root
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error("Root element not found");
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

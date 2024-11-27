f8d5e83a2b485576cd922552dbd8e759e34c7d9f
Morelle-Olivia <hotoumorelle@gmail.com>
2024-11-27 16:23:32 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="">
-      <header className="App-header">
-        <img src="logo" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="w-full center max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username" type="text" placeholder="Username"/>
+          </div>
+        </form>
+      </div>
+      {/*<header className="App-header">*/}
+        {/*<img src="logo" className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
+      {/*</header>*/}
     </div>
   );
 }

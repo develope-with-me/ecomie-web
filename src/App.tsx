0cb74efa851188d3732d7a16a8675520f5cd285a
Morelle-Olivia <hotoumorelle@gmail.com>
2024-12-23 12:47:17 +0100

    ec-t-1 | Added design for user profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import './App.css';
 import SignUp from './pages/Auth/SignUp';
 import Login from './pages/Auth/Login';
+import EvangelistPage from "./pages/Evangelist-page/evangelist-page";
+import UserProfilePage from "./pages/user-profile-page/user-profile-page";
+import React from 'react';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
       <Routes>
   <Route path="/signUp" element={ <SignUp/>} />
   <Route path="/" element={ <Login/>} />
+  <Route path="/ui/evangelist-page" element={ <EvangelistPage/>}
+
+  />
+  <Route path="/ui/evangelist-page/user-profile" element={ <UserProfilePage/>} />
 
   </Routes>
   </BrowserRouter>
---
+++ src/pages/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import DashboardComponent from "../component/dashboard/dashboard";
+
+type WrapperProps = {
+    children: React.ReactNode;
+};
+
+const Layout: React.FC<WrapperProps> = ({ children }) => {
+    return (
+        <div>
+            <div className="layout">
+                {/*<app-navbar></app-navbar>*/}
+                <div className="main-content h-screen flex flex-row">
+                    <DashboardComponent/>
+                    <div className="content flex-grow">
+                        {children}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Layout;
